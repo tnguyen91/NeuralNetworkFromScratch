52cde3e9b337da49c3d68bb8a4b1096e759119db
tnguyen91 <nmt090102@gmail.com>
2025-08-17 02:02:53 -0700

    clean up sigmoid and relu (vector)

--- include/ActivationFunctions.h
+++ include/ActivationFunctions.h
@@ -6,18 +6,12 @@
 
 namespace ActivationFunctions {
     double sigmoid(double x);
-    std::vector<double> sigmoid(const std::vector<double>& x);
-    
-    double relu(double x);
-    std::vector<double> relu(const std::vector<double>& x);
-    
-    std::vector<double> softmax(const std::vector<double>& x);
-    
-    double sigmoidDerivativeFromInput(double x);
     double sigmoidDerivative(double sigmoid_output);
     
+    double relu(double x);
     double reluDerivative(double x);
 
+    std::vector<double> softmax(const std::vector<double>& x);
 }
 
 #endif 
--- src/ActivationFunctions.cpp
+++ src/ActivationFunctions.cpp
@@ -1,29 +1,21 @@
 #include "../include/ActivationFunctions.h"
+#include <limits>
+#include <algorithm>
 
 namespace ActivationFunctions {
 
     double sigmoid(double x) {
         return 1.0 / (1.0 + std::exp(-x));
     }
-
-    std::vector<double> sigmoid(const std::vector<double>& x) {
-        std::vector<double> result(x.size());
-        for (size_t i = 0; i < x.size(); ++i) {
-            result[i] = sigmoid(x[i]);
-        }
-        return result;
+    double sigmoidDerivative(double sigmoid_output) {
+        return sigmoid_output * (1.0 - sigmoid_output);
     }
 
     double relu(double x) {
         return std::max(0.0, x);
     }
-
-    std::vector<double> relu(const std::vector<double>& x) {
-        std::vector<double> result(x.size());
-        for (size_t i = 0; i < x.size(); ++i) {
-            result[i] = relu(x[i]);
-        }
-        return result;
+    double reluDerivative(double x) {
+        return x > 0.0 ? 1.0 : 0.0;
     }
 
     std::vector<double> softmax(const std::vector<double>& x) {
@@ -46,18 +38,4 @@ namespace ActivationFunctions {
         }
         return expVals;
     }
-
-    double sigmoidDerivativeFromInput(double x) {
-        double sig = sigmoid(x);
-        return sig * (1 - sig);
-    }
-
-    double sigmoidDerivative(double sigmoid_output) {
-        return sigmoid_output * (1.0 - sigmoid_output);
-    }
-
-    double reluDerivative(double x) {
-        return x > 0.0 ? 1.0 : 0.0;
-    }
-
 }
