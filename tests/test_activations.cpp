5326d8138fe108feb23ba189363d1119c3aa4ed5
tnguyen91 <nmt090102@gmail.com>
2025-08-17 01:27:00 -0700

    clean up test cases

--- tests/test_activations.cpp
+++ tests/test_activations.cpp
@@ -97,46 +97,6 @@ TEST(ActivationFunctions, SoftmaxBasic) {
     
     EXPECT_EQ(result.size(), input.size());
     
-    for (double val : result) {
-        EXPECT_GT(val, 0.0) << "Softmax output should be positive";
-        EXPECT_LT(val, 1.0) << "Softmax output should be less than 1";
-        EXPECT_TRUE(std::isfinite(val)) << "Softmax output should be finite";
-    }
-    
-    double sum = 0.0;
-    for (double val : result) {
-        sum += val;
-    }
-    EXPECT_NEAR(sum, 1.0, 1e-10) << "Softmax outputs should sum to 1";
-}
-
-TEST(ActivationFunctions, SoftmaxEqualInputs) {
-    std::vector<double> input = {2.0, 2.0, 2.0, 2.0};
-    std::vector<double> result = ActivationFunctions::softmax(input);
-    
-    double expected = 1.0 / input.size();  // 1/4 = 0.25
-    
-    for (double val : result) {
-        EXPECT_NEAR(val, expected, 1e-10) << "Equal inputs should give uniform distribution";
-    }
-}
-
-TEST(ActivationFunctions, SoftmaxDominantInput) {
-    std::vector<double> input = {100.0, 1.0, 1.0};
-    std::vector<double> result = ActivationFunctions::softmax(input);
-    
-    EXPECT_GT(result[0], 0.99) << "Dominant input should get most probability";
-    
-    EXPECT_LT(result[1], 0.01) << "Non-dominant inputs should get low probability";
-    EXPECT_LT(result[2], 0.01) << "Non-dominant inputs should get low probability";
-}
-
-TEST(ActivationFunctions, SoftmaxNegativeInputs) {
-    std::vector<double> input = {-1.0, -2.0, -3.0};
-    std::vector<double> result = ActivationFunctions::softmax(input);
-    
-    EXPECT_EQ(result.size(), 3);
-    
     double sum = 0.0;
     for (double val : result) {
         EXPECT_GT(val, 0.0);
@@ -144,9 +104,6 @@ TEST(ActivationFunctions, SoftmaxNegativeInputs) {
         sum += val;
     }
     EXPECT_NEAR(sum, 1.0, 1e-10);
-    
-    EXPECT_GT(result[0], result[1]) << "-1 should have higher probability than -2";
-    EXPECT_GT(result[1], result[2]) << "-2 should have higher probability than -3";
 }
 
 TEST(ActivationFunctions, SoftmaxNumericalStability) {
@@ -154,8 +111,7 @@ TEST(ActivationFunctions, SoftmaxNumericalStability) {
     std::vector<double> result = ActivationFunctions::softmax(input);
     
     for (double val : result) {
-        EXPECT_TRUE(std::isfinite(val)) << "Softmax should handle large inputs stably";
-        EXPECT_FALSE(std::isnan(val)) << "Softmax should not produce NaN";
+        EXPECT_TRUE(std::isfinite(val));
     }
     
     double sum = 0.0;
@@ -163,23 +119,5 @@ TEST(ActivationFunctions, SoftmaxNumericalStability) {
         sum += val;
     }
     EXPECT_NEAR(sum, 1.0, 1e-10);
-}
-
-TEST(ActivationFunctions, SoftmaxSingleElement) {
-    std::vector<double> input = {5.0};
-    std::vector<double> result = ActivationFunctions::softmax(input);
-    
-    EXPECT_EQ(result.size(), 1);
-    EXPECT_NEAR(result[0], 1.0, 1e-10) << "Single element softmax should be 1.0";
-}
-
-TEST(ActivationFunctions, SoftmaxZeroInputs) {
-    std::vector<double> input = {0.0, 0.0, 0.0};
-    std::vector<double> result = ActivationFunctions::softmax(input);
-    
-    double expected = 1.0 / 3.0;
-    for (double val : result) {
-        EXPECT_NEAR(val, expected, 1e-10) << "Zero inputs should give uniform distribution";
-    }
 }
 
